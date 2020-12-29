4a0cc5f7c6f701c2f6e61b40e6b0e61ff0979d64
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2020-12-30 01:08:57 +0530

    :card_file_box: Add nested join in 'getUserById'

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,28 @@
 const User = require("../models/User");
 
+// TODO: Exclude
+
 /**
  * Get User by ID
  */
 exports.getUserById = (req, res) => {
   const userID = req.body.userID;
   User.findById(userID)
-    .populate("trips")
+    .populate({
+      path: "trips",
+      populate: {
+        path: "members",
+        model: "User",
+        select: {
+          trips: 0,
+          _id: 0,
+          googleId: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+        },
+      },
+    })
     .exec((err, user) => {
       if (err) {
         return res.status(400).json({ error: "Cannot fetch user" });
