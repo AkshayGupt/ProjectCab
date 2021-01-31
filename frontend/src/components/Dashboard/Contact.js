996acf90381100cddd56461556bc15c649009fb4
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-01-31 21:17:10 +0530

    :lipstick: Improve Contact Us Page

--- frontend/src/components/Dashboard/Contact.js
+++ frontend/src/components/Dashboard/Contact.js
@@ -1,37 +1,57 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Row, Col, Button, Image } from "react-bootstrap";
 import "./Contact.css";
 
 const Contact = () => {
   return (
-    <Container className="container-custom">
-      <Form>
-        <h1 className="mt-5">Get in touch</h1>
-        <div className="subtitle">
-          <h6>Have an inquiry or some feedback for us?</h6>
-          <h6>Fill out the form below to contact out team.</h6>
-        </div>
-        <Form.Group>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control placeholder="John" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control placeholder="Doe" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email Id</Form.Label>
-          <Form.Control placeholder="john@doe.com" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Message</Form.Label>
-          <Form.Control placeholder="Hello, I just wanted to report an error..." />
-        </Form.Group>
-      </Form>
-      <Button variant="primary" type="submit">
-        Get in touch
-      </Button>
-    </Container>
+    <Row className="custom-padding">
+      <Col className="d-none d-lg-block">
+        <Image
+          src="https://images.unsplash.com/photo-1489824904134-891ab64532f1"
+          fluid
+        />
+      </Col>
+      <Col>
+        <Container>
+          <Form>
+            <h1 className="mt-5">Get in touch</h1>
+            <div className="subtitle">
+              <h6>Have an inquiry or some feedback for us?</h6>
+              <h6>Fill out the form below to contact out team.</h6>
+            </div>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" placeholder="John" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="Doe" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group>
+              <Form.Label>Email Id</Form.Label>
+              <Form.Control type="text" placeholder="john@doe.com" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Hello, I just wanted to report an error..."
+              />
+            </Form.Group>
+          </Form>
+          <Button variant="primary" type="submit">
+            Get in touch
+          </Button>
+        </Container>
+      </Col>
+    </Row>
   );
 };
 
