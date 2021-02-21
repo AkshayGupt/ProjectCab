be40b39107e5d91bb75a9e5fa7685c3857105644
AkshayGupt <189301019.akshay@gmail.com>
2021-02-21 14:51:58 +0530

    Removing unwanted files and warnings

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () =>{
         const connection = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
+            useFindAndModify: false,
+            useCreateIndex:true
         })
 
         console.log(`MongoDB Connected: ${connection.connection.host}`)
